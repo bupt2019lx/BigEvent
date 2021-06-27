514ef2623ee7cd276d26e01dfba61cf31c092182
lixin <773549436@qq.com>
2021-06-27 15:00:12 +0800

    登录接口完成

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.listen(3000, () => {
 })
 const db = require('./db')
 const md5 = require('md5');
+
+// ------------------------------------------------------注册接口-------------------------------------------------------------------
 app.use(express.urlencoded({
     extended: true
 }))
@@ -32,4 +34,27 @@ app.post('/api/reguser', (req, res) => {
     })
 
 
+})
+
+//-------------------------------------------------------登录接口----------------------------------------------------------
+app.post('/api/login', (req, res) => {
+    let {
+        username,
+        password
+    } = req.body;
+    password = md5(password);
+    db(`select * from user where username='${username}' and password='${password}'`, (err, result) => {
+        if (err) throw err
+        if (result.length > 0) {
+            res.send({
+                status: 0,
+                message: '登录成功'
+            })
+        } else {
+            res.send({
+                status: 1,
+                message: '登录失败'
+            })
+        }
+    })
 })
